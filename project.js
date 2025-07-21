977f4efdde20a7ff8ef6fc1a608f596d7845b70e
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 23:01:09 -0500

    update

--- project.js
+++ project.js
@@ -1,1 +1,35 @@
-a
+let sentOne = ("!yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll\'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC");
+let arrOne = sentOne.split(""); 
+console.log(arrOne);
+arrOne.sort();
+arrOne.reverse();
+console.log(arrOne);
+updatedSent = arrOne.toString();
+console.log(updatedSent);
+
+let sentTwo = ("ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB");
+let arrTwo = sentOne.split(""); 
+console.log(arrOne);
+arrOne.sort();
+arrOne.reverse();
+console.log(arrOne);
+updatedSent = arrOne.toString();
+console.log(updatedSent);
+
+let sentThree = ("!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI.lanoisseforp a ekil leef ot evah t'nod uoY");
+let arrThree = sentOne.split(""); 
+console.log(arrOne);
+arrOne.sort();
+arrOne.reverse();
+console.log(arrOne);
+updatedSent = arrOne.toString();
+console.log(updatedSent);
+
+let sentFour = (".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ");
+let arrFour = sentOne.split(""); 
+console.log(arrOne);
+arrOne.sort();
+arrOne.reverse();
+console.log(arrOne);
+updatedSent = arrOne.toString();
+console.log(updatedSent);
